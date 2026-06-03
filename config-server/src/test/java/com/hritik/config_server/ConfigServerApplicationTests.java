55b9d4b0a6f4d7c2ff1777ce65e1f472423441cb
Hritik Singh <hritikk99.99@gmail.com>
2026-06-03 07:56:17 +0530

    Added configServer service and updated its application.properties

--- config-server/src/test/java/com/hritik/config_server/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/hritik/config_server/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.hritik.config_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
